c9c42bf9fe0a74e94fae0c2f651d766c51c5fb59
ynaka <ymnkyj@gmail.com>
2017-12-30 17:33:00 +0900

    バージョン管理するファイルを追加した。

---
+++ nigaC/nigaC/nigaC.cpp
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int main(void)
+{
+	
+	//cout << SCHAR_MIN << endl;
+	//cout << SCHAR_MIN << endl;
+	//cout << SCHAR_MIN << endl;
+	//cout << SCHAR_MIN << endl;
+	//cout << SCHAR_MIN << endl;
+	//printf("SCHAR_MIN\t: %d\n", SCHAR_MIN);
+	//printf("%d\n", CHAR_MIN);
+	//printf("%d\n", SHRT_MIN);
+	//printf("%d\n", INT_MIN);
+	//printf("%d\n", LONG_MIN);
+	//printf("%lld\n", LLONG_MIN);
+	cout << "SCHAR_MIN\t: " << SCHAR_MIN << endl;
+	cout << "CHAR_MIN\t: " << CHAR_MIN << endl;
+	cout << "SHRT_MIN\t: " << SHRT_MIN << endl;
+	cout << "INT_MIN\t\t: " << INT_MIN << endl;
+	cout << "LONG_MIN\t: " << LONG_MIN << endl;
+	cout << "LLONG_MIN\t: " << LLONG_MIN << endl;
+
+	/*printf("%d\n", UCHAR_MAX);
+	printf("%d\n", SCHAR_MAX);
+	printf("%d\n", CHAR_MAX);
+	printf("%d\n", USHRT_MAX); 
+	printf("%d\n", SHRT_MAX);	
+	printf("%d\n", UINT_MAX);*/
+	cout << "UCHAR_MAX\t: " << UCHAR_MAX << endl;
+	cout << "SCHAR_MAX\t: " << SCHAR_MAX << endl;
+	cout << "CHAR_MAX\t: " << CHAR_MAX << endl;
+	cout << "USHRT_MAX\t: " << USHRT_MAX << endl;
+	cout << "SHRT_MAX\t: " << SHRT_MAX << endl;
+	cout << "UINT_MAX\t: " << UINT_MAX << endl;
+	cout << "INT_MAX\t\t: " << INT_MAX << endl;
+	cout << "ULONG_MAX\t: " << ULONG_MAX << endl;
+	cout << "LONG_MAX\t: " << LONG_MAX << endl;
+	cout << "ULLONG_MAX\t: " << ULLONG_MAX << endl;
+	cout << "LLONG_MAX\t: " << LLONG_MAX << endl;
+	/*printf("%d\n", INT_MAX);
+	printf("%d\n", ULONG_MAX);
+	printf("%d\n", LONG_MAX);
+	printf("%lld\n", ULLONG_MAX);
+	printf("%lld\n", LLONG_MAX);*/
+
+	cout << "CHAR_BIT\t: " << CHAR_BIT << endl;
+	cout << "MB_LEN_MAX\t: " << MB_LEN_MAX << endl;
+	
+
+	return 0;
+}
